3a021c19cbb0b662ea3b6d3c056cb645370fe80e
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-06-17 16:27:14 +0530

    Create Nor_tb.v

---
+++ Day1/Nor_gate/Nor_tb.v
@@ -0,0 +1,17 @@
+module Nor_tb();
+reg a, b;
+wire c;
+Nor uut(.a(a),.b(b),.c(c));
+
+initial begin
+
+$dumpfile("Nor_tb.vcd");
+$dumpvars();
+$monitor($time,"a=%b,b=%b,c=%b",a,b,c); 
+     a=0; b=0;
+#10  a=0; b=1;
+#10  a=1; b=0;
+#10  a=1; b=1;
+$finish;
+end 
+endmodule
