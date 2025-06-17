f91aff6555e860787440f9651b5b3ced022b5a3a
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-06-17 16:09:33 +0530

    Create Nand_tb.v

---
+++ Day1/Nand_gate/Nand_tb.v
@@ -0,0 +1,17 @@
+module Nand_tb();
+reg a,b;
+wire c;
+Nand uut(.a(a),.b(b),.c(c));
+
+initial begin
+
+$dumpfile("Nand_tb.vcd");
+$dumpvars();
+$monitor($time,"a=%b,b=%b,c=%b",a,b,c);
+#10 a=0; b=0;
+#10 a=0; b=1;
+#10 a=1; b=0;
+#10 a=1; b=1;
+$finish;
+end
+endmodule
