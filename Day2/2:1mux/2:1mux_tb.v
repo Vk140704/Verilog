96ab985c016cc9111b5c0db8fa6096a9b7c68fbe
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-06-23 16:14:09 +0530

    Update 2:1mux_tb.v

--- Day2/2:1mux/2:1mux_tb.v
+++ Day2/2:1mux/2:1mux_tb.v
@@ -1,8 +1,13 @@
-module mux2_1(input i0,i1,s,output reg y);
-always@(*)begin
-case(s)
-1'b0: y=i0;
-1'b1: y=i1;
-endcase
-end
+module mux2_1tb();
+reg i0,i1,s;
+wire y;
+mux2_1 utt(.i0(i0),.i1(i1),.s(s),.y(y));
+initial begin
+$dumpfile("mux2_1tb.vcd");
+$dumpvars();
+$monitor(" %0t i0=%b,i1=%b,s=%b,y=%b" ,$time,i0,i1,s,y);
+#10 i0=0; i1=1;
+#10 s=0;
+#10 s=1;
+end 
 endmodule
